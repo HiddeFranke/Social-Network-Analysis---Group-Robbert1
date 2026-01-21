910f3dff5d1394a1efc1bf15eddf22d3be52ddfd
HiddeFranke <hiddefranke@gmail.com>
2026-01-21 16:56:13 +0100

    Update 5_kemeny_interactive.py

--- src/dss/pages/5_kemeny_interactive.py
+++ src/dss/pages/5_kemeny_interactive.py
@@ -581,7 +581,9 @@ def page() -> None:
 
         if order:
             st.markdown("### Kemeny constant after each removal", help="""
-                    The order of the removal of certain edges has an impact on the subsequent Kemeny values that the remaining edges contain. A certain edge can have a bigger impact on the information network if similar edges have already been removed. As such, the following graph shows the inpact of each edge removal per step.
+                    The order of the removal of certain edges has an impact on the subsequent Kemeny values that the remaining edges contain. 
+                    A certain edge can have a bigger impact on the information network if similar edges have already been removed. 
+                    As such, the following graph shows the inpact of each edge removal per step.
             """)
             fig, ax = plt.subplots()
             series = [base_k] + result.history
